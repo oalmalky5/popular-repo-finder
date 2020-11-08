9fc1613f90f171077ccb8e2e9a57574628d8695a
oalmalky5 <oalmalky5@gmail.com>
2020-11-08 17:41:53 +0300

    finishing the navebar

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,21 +1,67 @@
 import React from 'react'
+import PropType from 'prop-types'
 
 
+
+
+const LanguagesNav = ({selected, onUpdateLanguage}) => {
+    const langs = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'] 
+    return (
+        // this is more or less how you loop over a list in react  
+        <ul className='flex-center'>
+            {langs.map((language) => (
+                <li key={language}> 
+                    <button 
+                        style={language === selected? {color: 'brown'} : null } // inline style in jsx
+                        onClick={() => onUpdateLanguage(language)} 
+                        className='btn-clear nav-link'
+                    >
+                        {language}
+                     </button>
+                </li>
+            ))}
+        </ul>
+    )
+
+}
+
+LanguagesNav.prototype = {
+    selected: PropType.string.isRequired,
+    onUpdateLanguage: PropType.func.isRequired
+}
 class Popular extends React.Component{
-    render() {
 
-        const langs = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'] 
+    constructor(props){
+        super(props)
+
+        this.state = {
+            selectedLang: 'All'
+        }
+
+        // its good habit to bind methods whenever creating one like this
+        // to avoid the common undefined setStatet problem
+        this.updatedLang = this.updatedLang.bind(this)
+    }
+
+    updatedLang(selectedLang) {
+        this.setState({
+            selectedLang
+             
+        })
+        // console.log('lang selcted')
+    } 
+
+
+    render() {
+        const {selectedLang} = this.state
+
         return (
-            // this is more or less how you loop over a list in react  
-            <ul className='flex-center'>
-                {langs.map((item, index) => (
-                    <li key={index}> 
-                        <button className='btn-clear nav-link'>
-                            {item}
-                        </button>
-                    </li>
-                ))}
-            </ul>
+            <React.Fragment>
+                <LanguagesNav 
+                    selected={selectedLang}
+                    onUpdateLanguage={this.updatedLang}
+                />
+            </React.Fragment>
         )
     }
 }
